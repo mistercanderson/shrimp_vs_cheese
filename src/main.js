7db4c0bc49b29721bf05e96b1238fc85db5e6898
Chris Anderson <mistercanderson@gmail.com>
2021-03-20 14:01:36 -0600

    Add/remove semicolons

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ var game;
 var player;
 
 // DOM Elements
-var gameSection = document.getElementById('game')
+var gameSection = document.getElementById('game');
 var gameDisplay = document.getElementById('gameDisplay');
 var gameBoard = document.getElementById('gameBoard');
 var spaces = gameBoard.children;
 var player1Column = document.getElementById('playerOne');
-var player2Column = document.getElementById('playerTwo')
+var player2Column = document.getElementById('playerTwo');
 
 // Event Listeners
 window.addEventListener('load', checkStorage);
@@ -20,8 +20,8 @@ function createGame(player1, player2) {
   var p1 = new Player(player1, true);
   var p2 = new Player(player2);
   ticTacToe.addPlayers(p1, p2);
-  return ticTacToe
-};
+  return ticTacToe;
+}
 
 function startButton() {
   if (event.target.id === 'startButton') {
@@ -29,38 +29,38 @@ function startButton() {
     player = checkCurrentPlayer();
     indicateCurrentPlayer();
     hideButtons();
-  };
-};
+  }
+}
 
 function clear() {
-  if (event.target.id === "clearButton") {
+  if (event.target.id === 'clearButton') {
     if (window.confirm('Are you sure you want to clear the saved game?')) {
       if (window.confirm('Like, seriously?')) {
-        window.alert('THE WATERS HAVE BEEN CLEANSED')
+        window.alert('THE WATERS HAVE BEEN CLEANSED');
         load();
         localStorage.clear();
         checkStorage();
-        event.target.style.color = 'coral'
-      };
-    };
-  };
-};
+        event.target.style.color = 'coral';
+      }
+    }
+  }
+}
 
 function hideButtons() {
   var buttons = document.querySelectorAll('button');
   for (var i = 0; i < buttons.length; i++) {
     buttons[i].style.display = 'none';
-  };
+  }
   player1Column.style.opacity = 1;
   player2Column.style.opacity = 1;
-};
+}
 
 function checkStorage() {
   if (localStorage.length < 1) {
-    document.getElementById('loadButton').disabled = true
-    document.getElementById('clearButton').disabled = true
-  };
-};
+    document.getElementById('loadButton').disabled = true;
+    document.getElementById('clearButton').disabled = true;
+  }
+}
 
 function buttonFunctions() {
   startButton();
@@ -69,15 +69,15 @@ function buttonFunctions() {
   playAgain();
   reset();
   saveGame();
-};
+}
 
 function checkEmptySpace() {
   for (var i = 0; i < spaces.length; i++) {
     if (!spaces[i].innerHTML) {
-      hideButtons()
-    };
-  };
-};
+      hideButtons();
+    }
+  }
+}
 
 function playGame() {
   buttonFunctions();
@@ -93,9 +93,9 @@ function playGame() {
       gameDraw();
     } else {
       renderBoard();
-    };
-  };
-};
+    }
+  }
+}
 
 function gameWon() {
   if (game.isWon) {
@@ -113,8 +113,8 @@ function gameWon() {
       resetButton();
       saveButton();
     }, 2500);
-  };
-};
+  }
+}
 
 function gameDraw() {
   if (game.isDraw) {
@@ -130,46 +130,46 @@ function gameDraw() {
       resetButton();
       saveButton();
     }, 2500);
-  };
-};
+  }
+}
 
 function move(player, space) {
   if (!game.board[space]) {
     game.makeMove(player, space);
-  };
-};
+  }
+}
 
 function checkCurrentPlayer() {
   for (var i = 0; i < game.players.length; i++) {
     if (game.players[i].isTurn) {
       return game.players[i];
-    };
-  };
-};
+    }
+  }
+}
 
 function checkCurrentSpace() {
   var currentSpace;
   for (var i = 0; i < spaces.length; i++) {
     if (event.target === spaces[i]) {
       currentSpace = i;
-    };
-  };
-  return currentSpace
-};
+    }
+  }
+  return currentSpace;
+}
 
 function checkWinner() {
   return game.checkForWin(player);
-};
+}
 
 function changeGameHeader(player) {
   if (checkWinner()) {
     gameDisplay.innerText = checkWinner().toUpperCase();
     wobbleText();
-    return
-  };
+    return;
+  }
   gameDisplay.innerText = 'SHRIMP VS. CHEESE';
   wobbleText();
-};
+}
 
 function indicateCurrentPlayer() {
   var activePlayer = checkCurrentPlayer();
@@ -179,48 +179,48 @@ function indicateCurrentPlayer() {
   var dotTwo = document.getElementById('playerTwoDot');
   if (activePlayer.token === 'shrimp' && !game.isWon && !game.isDraw) {
     shrimp.classList.add('wiggle');
-    cheese.classList.remove('wiggle')
+    cheese.classList.remove('wiggle');
     dotOne.classList.add('player-one-dot');
     dotTwo.classList.remove('player-two-dot');
   } else if (activePlayer.token === 'cheese' && !game.isWon && !game.isDraw) {
     shrimp.classList.remove('wiggle');
-    cheese.classList.add('wiggle')
+    cheese.classList.add('wiggle');
     dotOne.classList.remove('player-one-dot');
     dotTwo.classList.add('player-two-dot');
   } else {
     shrimp.classList.remove('wiggle');
-    cheese.classList.remove('wiggle')
+    cheese.classList.remove('wiggle');
     dotOne.classList.remove('player-one-dot');
     dotTwo.classList.remove('player-two-dot');
-  };
-};
+  }
+}
 
 function gameEndAnimation() {
-  indicateCurrentPlayer()
+  indicateCurrentPlayer();
   if (game.isWon) {
     for (var i = 0; i < spaces.length; i++) {
       if (spaces[i].children[0] &&
         spaces[i].children[0].className === player.token) {
-        spaces[i].children[0].classList.add('winner')
-      };
-    };
+        spaces[i].children[0].classList.add('winner');
+      }
+    }
   } else if (game.isDraw) {
     for (var i = 0; i < spaces.length; i++) {
-      spaces[i].children[0].classList.add('draw')
-    };
-  };
-};
+      spaces[i].children[0].classList.add('draw');
+    }
+  }
+}
 
 function renderBoard() {
   for (var i = 0; i < game.board.length; i++) {
     spaces[i].innerHTML = '';
     if (game.board[i] === 'shrimp') {
-      spaces[i].innerHTML += '<img class="shrimp" src="./assets/shrimp.svg" alt="shrimp">'
+      spaces[i].innerHTML += '<img class="shrimp" src="./assets/shrimp.svg" alt="shrimp">';
     } else if (game.board[i] === 'cheese') {
-      spaces[i].innerHTML += '<img class="cheese" src="./assets/cheese.svg" alt="cheese">'
-    };
-  };
-};
+      spaces[i].innerHTML += '<img class="cheese" src="./assets/cheese.svg" alt="cheese">';
+    }
+  }
+}
 
 function clearBoard() {
   indicateCurrentPlayer();
@@ -228,22 +228,22 @@ function clearBoard() {
   if (game.isWon) {
     displayWinnerMobile(player);
     game.resetBoard();
-    changeGameHeader(player)
+    changeGameHeader(player);
     renderBoard();
   } else {
     game.resetBoard();
-    changeGameHeader(player)
+    changeGameHeader(player);
     renderBoard();
-  };
-};
+  }
+}
 
 function disableBoard() {
-  gameBoard.classList.add('disable-board')
-};
+  gameBoard.classList.add('disable-board');
+}
 
 function enableBoard() {
-  gameBoard.classList.remove('disable-board')
-};
+  gameBoard.classList.remove('disable-board');
+}
 
 function displayWinTotal(player) {
   var playerOneWins = document.getElementById('playerOneWins');
@@ -255,97 +255,97 @@ function displayWinTotal(player) {
     } else if (player.token === 'cheese') {
       playerTwoWins.innerHTML = '';
       playerTwoWins.innerHTML += `<p>${player.winTotal}</p>`;
-    };
+    }
   } else {
     playerOneWins.innerHTML = '';
     playerTwoWins.innerHTML = '';
-  };
-};
+  }
+}
 
 function displayWinnerMobile(player) {
   if (player.token === 'shrimp') {
-    player1Column.classList.add('mobile-win')
-    player2Column.classList.remove('mobile-win')
+    player1Column.classList.add('mobile-win');
+    player2Column.classList.remove('mobile-win');
   } else if (player.token === 'cheese') {
-    player1Column.classList.remove('mobile-win')
-    player2Column.classList.add('mobile-win')
-  };
+    player1Column.classList.remove('mobile-win');
+    player2Column.classList.add('mobile-win');
+  }
   setTimeout(function() {
-    hideWinMobile()
-  }, 1500)
-};
+    hideWinMobile();
+  }, 1500);
+}
 
 function hideWinMobile() {
   player1Column.classList.remove('mobile-win');
-  player2Column.classList.remove('mobile-win')
-};
+  player2Column.classList.remove('mobile-win');
+}
 
 function wobbleText() {
   if (gameDisplay.innerText !== 'SHRIMP VS. CHEESE') {
-    gameDisplay.classList.add('wobble')
+    gameDisplay.classList.add('wobble');
   } else {
-    gameDisplay.classList.remove('wobble')
-  };
-};
+    gameDisplay.classList.remove('wobble');
+  }
+}
 
 function playAgainButton() {
   var topSquare = document.getElementById('topCenter');
-  topSquare.innerHTML += `<button class="again" id="againButton">AGAIN?</button>`;
-};
+  topSquare.innerHTML += '<button class="again" id="againButton">AGAIN?</button>';
+}
 
 function playAgain() {
   if (event.target.id === 'againButton') {
     indicateCurrentPlayer();
     hideButtons();
-  };
-};
+  }
+}
 
 function resetButton() {
   var bottomLeftSquare = document.getElementById('bottomLeft');
-  bottomLeftSquare.innerHTML += `<button class="reset" id="resetButton">RESET</button>`
-};
+  bottomLeftSquare.innerHTML += '<button class="reset" id="resetButton">RESET</button>';
+}
 
 function reset() {
   if (event.target.id === 'resetButton') {
     for (var i = 0; i < game.players.length; i++) {
       game.players[i].winTotal = 0;
       game.players[i].wins = [];
-      displayWinTotal(game.players[i])
-    };
+      displayWinTotal(game.players[i]);
+    }
     indicateCurrentPlayer();
     hideButtons();
-  };
-};
+  }
+}
 
 function saveButton() {
   var bottomRightSquare = document.getElementById('bottomRight');
-  bottomRightSquare.innerHTML += `<button id="saveButton">SAVE</button>`
-};
+  bottomRightSquare.innerHTML += '<button id="saveButton">SAVE</button>';
+}
 
 function save() {
   for (var i = 0; i < game.players.length; i++) {
-    localStorage.setItem(`${game.players[i].token}`, JSON.stringify(game.players[i]))
-  };
+    localStorage.setItem(`${game.players[i].token}`, JSON.stringify(game.players[i]));
+  }
   event.target.innerText = 'SAVED';
-  event.target.style.color = 'coral'
+  event.target.style.color = 'coral';
   event.target.disabled = true;
-};
+}
 
 function confirmSave() {
   if (window.confirm('This will overwrite previously stored game data. Are you sure?')) {
-    save()
-  };
-};
+    save();
+  }
+}
 
 function saveGame() {
   if (event.target.id === 'saveButton') {
     if (localStorage.length) {
       confirmSave();
     } else {
-      save()
-    };
-  };
-};
+      save();
+    }
+  }
+}
 
 function load() {
   if (event.target.id === 'loadButton') {
@@ -353,19 +353,19 @@ function load() {
     var playerProfiles = [];
     for (var i = 0; i < keys.length; i++) {
       var playerData = JSON.parse(localStorage.getItem(keys[i]));
-      playerProfiles.push(playerData)
-    };
+      playerProfiles.push(playerData);
+    }
     game = createGame('shrimp', 'cheese');
     for (var i = 0; i < game.players.length; i++) {
       game.players[i].retrieveDataFromStorage(playerProfiles[i]);
-    };
+    }
     displayWinTotalOnLoad();
     indicateCurrentPlayer();
     hideButtons();
   } else if (event.target.id === 'clearButton') {
     game = createGame('shrimp', 'cheese');
-  };
-};
+  }
+}
 
 function displayWinTotalOnLoad() {
   var shrimp;
@@ -375,14 +375,14 @@ function displayWinTotalOnLoad() {
       shrimp = game.players[i];
     } else if (game.players[i].token === 'cheese') {
       cheese = game.players[i];
-    };
-  };
+    }
+  }
   if (shrimp.winTotal) {
     var playerOneCounter = document.getElementById('playerOneWins');
-    playerOneCounter.innerHTML += `<p>${shrimp.winTotal}</p>`
-  };
+    playerOneCounter.innerHTML += `<p>${shrimp.winTotal}</p>`;
+  }
   if (cheese.winTotal) {
     var playerTwoCounter = document.getElementById('playerTwoWins');
-    playerTwoCounter.innerHTML += `<p>${cheese.winTotal}</p>`
-  };
-};
+    playerTwoCounter.innerHTML += `<p>${cheese.winTotal}</p>`;
+  }
+}
--- src/player.js
+++ src/player.js
@@ -6,27 +6,27 @@ class Player {
     this.isTurn = isTurn || false;
     this.wins = [];
     this.winTotal = 0;
-  };
+  }
 
   saveWinsToStorage(game) {
     var board = Array.from(game.board);
     this.wins.push(board);
-    this.winTotal++
-  };
+    this.winTotal++;
+  }
 
   retrieveDataFromStorage(playerProfile) {
     this.id = playerProfile.id;
     this.token = playerProfile.token;
     this.isTurn = playerProfile.isTurn;
     this.wins = playerProfile.wins;
-    this.winTotal = playerProfile.winTotal
-  };
+    this.winTotal = playerProfile.winTotal;
+  }
 
   exchangeTurns() {
     if (!this.isTurn) {
       this.isTurn = true;
     } else {
       this.isTurn = false;
-    };
-  };
-};
+    }
+  }
+}
